b46e8e68651cfc705a1761189b8259935833b66f
slavykov <slavykpota2@gmail.com>
2025-10-04 13:11:54 +0300

    Refactor GUI code for to-do application to improve input handling and layout structure

--- gui.py
+++ gui.py
@@ -2,9 +2,30 @@ import functions
 import FreeSimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo")
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 
-window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button]])
-window.read()
+window = sg.Window('My To-Do App',
+                   layout=[[label],
+                           [input_box, add_button]],
+                   font=('Helvetica', 20))
+
+
+while True:
+    result = window.read()
+    if result is None:
+        break
+    event, values = result
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
+
 window.close()
